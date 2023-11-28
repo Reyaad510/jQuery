ac10da372f3e694b7f38ecb854235928fbbb3782
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2023-11-27 21:32:15 -0500

    Notes on jQuery.ready and minification

---
+++ index.js
@@ -0,0 +1,15 @@
+// "$" is jquery. Place the script tag above the javascript script in "index.html", or won't work properly bcuz browser read code from top to bottom so will try to process index.js and wouldnt have gotten to the jquery script yet. So has to be above it!S
+
+// do below if jquery script is not at end of index.html!
+/*
+$(document).ready(function(){
+
+    $("h1").css("color", "red");
+
+})
+*/
+
+
+// Minifiy is when you take the jquery or other script and post in google and it has all the code not seperated and it helps reduce file size. Can use www.minifier.org to reduce any javascript or css file! Will help site load faster!
+
+
