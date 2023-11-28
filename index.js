f0ac7d2b40946f6e70a5c13627a1c8b572bea354
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2023-11-27 21:49:44 -0500

    Manipulating styles with jQuery notes

--- index.js
+++ index.js
@@ -13,3 +13,36 @@ $(document).ready(function(){
 // Minifiy is when you take the jquery or other script and post in google and it has all the code not seperated and it helps reduce file size. Can use www.minifier.org to reduce any javascript or css file! Will help site load faster!
 
 
+
+// Selecting elements with jQuery
+// can select one element using below
+$("h1.title")
+
+
+// Can select multiple elements with writing the same way as below
+// unlike where you had to do querySelectAll()
+$("button")
+
+
+
+// Manipulate styles with jQuery
+// console.log(${("h1").css("font-size")})
+//$("h1").css("color","green");
+
+
+// adding class from styles.css to h1
+// $("h1").addClass("big-title");
+
+
+// remove class 
+// $("h1").removeClass("big-title");
+
+
+// adding multiple classes in one line
+// $("h1").addClass("big-title margin-50");
+
+
+// check if something has a class
+// $("h1").hasClass("margin-50"); true/false
+
+
